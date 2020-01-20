ba4f1f2307a61cd21a8b0ce7f9986c36d32da5bd
Kevin <bubbagump2020@gmail.com>
2020-01-20 11:03:08 -0600

    Added redux-persist

--- recipe-book-frontend/src/redux/stores/index.js
+++ recipe-book-frontend/src/redux/stores/index.js
@@ -1,7 +1,18 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { recipeBook } from '../reducers/index'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, recipeBook)
+
+
 export const store = createStore(
-    recipeBook, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+export const persistor = persistStore(store)
